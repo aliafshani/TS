a42883b4649086d4a3d4261338f4d544e59698d7
alish <alifshania222@gmail.com>
2024-11-07 06:03:06 +0330

    first commit

---
+++ sessions/1session/app.js
@@ -0,0 +1,39 @@
+// const newUser: string = 'ali afshani'
+// let user: { id: number, username: string, age: number } = {
+//     id: 1,
+//     username: "alireza"
+// }
+// console.log(user.id);
+/////////////////////////////////////////////////////
+// let users: string[] = ['ali', 'amin', 'mamad']
+// let randoms: (number | boolean | string)[] = [12, 'mamas', false]
+// users.push("niana")
+// console.log(users);
+/////////////////////////////////////////////////////
+// let roles: (string | number)[][] = [
+//     [1, 'admin'],
+//     [2, 'user']
+// ]
+//////////////////////////////////////////////////////
+// TUPPles
+// let role: [number, string] = [
+//     1, "admin"
+// ]
+// //////////////////////////////////////////////////////
+// function isLogin(username: string, password: number): (boolean | number) {
+//     if (username === "admin" && password == 1212) {
+//         return true
+//     } else {
+//         return 56;
+//     }
+// }
+// console.log(isLogin);
+// ///////////////////////////////////////////////////////////////////
+// let testVariables: any = 12
+///////////////////////////////////////////////////////////////
+// let age: number = 18
+// let age: 18 = 18
+// let age: 18 | 20 | 21 = 18
+// const useParamCss = (param: 'px' | 'em' | 'rem') => {
+//     return `20${param}`
+// }
